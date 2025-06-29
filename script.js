1fec59462a71847d2f8762ecd6e4a1816a2cee9f
Gavini Fanidhar <developerfanidhar@gmail.com>
2025-06-29 17:36:25 +0530

    Added cards in Hero Section, Made Navigation bar responsive.

---
+++ script.js
@@ -0,0 +1,8 @@
+function showSideBar(){
+    const sideBar = document.querySelector(".navigation-sidebar");
+    sideBar.style.display = "flex";
+}
+function hideSideBar(){
+    const sideBar = document.querySelector(".navigation-sidebar");
+    sideBar.style.display = "none";
+}
