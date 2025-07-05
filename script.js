2aee8543f7cf7db2d8ab71ad833c2169557d162e
Gavini Fanidhar <developerfanidhar@gmail.com>
2025-07-05 20:55:22 +0530

    Added new images and changed cards styles & it's logic.

--- script.js
+++ script.js
@@ -3,13 +3,14 @@
 // Description: Handles sidebar navigation for responsive UI.
 
 // Show the sidebar navigation (for mobile)
-function showSideBar(){
+function showSideBar() {
     const sideBar = document.querySelector(".navigation-sidebar");
     sideBar.style.display = "flex";
 }
 
 // Hide the sidebar navigation (for mobile)
-function hideSideBar(){
+function hideSideBar() {
     const sideBar = document.querySelector(".navigation-sidebar");
     sideBar.style.display = "none";
 }
+
